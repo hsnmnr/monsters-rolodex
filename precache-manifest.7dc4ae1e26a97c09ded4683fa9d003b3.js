0bf1f43d591ee8176550c753e2fdee83a57fc264
Hassan Munir <mehassanmunirch@gmail.com>
2019-08-23 21:11:13 +0500

    Updates

---
+++ precache-manifest.7dc4ae1e26a97c09ded4683fa9d003b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7d24dff503c1c205ed3e9537431d4f4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5a89323e59cad94fa53f",
+    "url": "/monsters-rolodex/static/css/main.0dc61b78.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5a89323e59cad94fa53f",
+    "url": "/monsters-rolodex/static/js/main.61e78e24.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
